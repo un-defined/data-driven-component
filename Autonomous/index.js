a66cb1740ad43a41d9f390af9dba0001d7cc2361
un-defined <659168590@qq.com>
2017-07-04 23:04:04 +0800

    modify layout algorithm

--- Autonomous/index.js
+++ Autonomous/index.js
@@ -1,16 +1,17 @@
 /**
  * Data source driven component.
- * 
+ *
  * @author: Andy
  * @time: 2017-06-28
  */
 
+import './index.css';
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {Form, Select, Input, Row, Col} from 'antd';
-
-const FormItem = Form.Item;
-const Option = Select.Option;
+import {Form, Select, Input, Row, Col, Radio, DatePicker} from 'antd';
+const FormItem   = Form.Item;
+const Option     = Select.Option;
+const RadioGroup = Radio.Group;
 
 class Autonomous extends Component{
     constructor (props) {
@@ -18,16 +19,30 @@ class Autonomous extends Component{
         // Binding `this`.
         this.genRows = this.genRows.bind(this);
     }
-    
+
     // Generate each row.
     genRows () {
         const _this = this;
-        const { matrix } = _this.props.data;
+        const { matrix, fields } = _this.props.data;
         return matrix.map( (row, ridx) => {
             // decide each column's span by column's count.
-            const colSpan = 24 / row.length;
+            let undefinedCount = 0;
+            let remain = 24;
+            let cols = row.map( col => {
+                let f = fields[col];
+                if( !f || !f.span ) {
+                    undefinedCount += 1;
+                    return undefined;
+                } else {
+                    remain -= f.span;
+                    return f.span;
+                }
+            });
+            let defaultSpan = remain / undefinedCount;
+            cols = cols.slice().map( c => c ? c : defaultSpan );
+            
             return <Row key={ridx}>{
-                row.map( (col, cidx) => <Col span={colSpan} key={cidx}>{
+                row.map( (col, cidx) => <Col span={cols[cidx]} key={cidx}>{
                     _this.genCol(col)
                 }</Col> )
             }</Row>
@@ -37,22 +52,31 @@ class Autonomous extends Component{
     // Generate each column.
     genCol (col) {
         if (!col) return <span></span>;
-        
+
         const _this = this;
         const { fields } = _this.props.data;
-        const formItemLayout = { labelCol: {span: 8}, wrapperCol: {span: 16} };
         const field = fields[col];
+        const formItemLayout = field.layout ? 
+            { labelCol: {span: field.layout[0]}, wrapperCol: {span: field.layout[1]} } :
+            { labelCol: {span: 8}, wrapperCol: {span: 16} };
         const visible = (('visible' in field) && !field.visible) ? 'hidden' : 'visible';
 
-        return <FormItem {...formItemLayout} label={field.label} style={{visibility: visible}}>{
-            field.children ? 
-            <Row>{
-                field.children.map( (child, idx) =>  
-                    <Col key={idx} span={fields[child].span}>
-                        <FormItem>{_this.genDecoratedField(child)}</FormItem>
-                    </Col> )
-            }</Row> : _this.genDecoratedField(col)
-        }</FormItem>
+        return (
+            <FormItem 
+                {...formItemLayout} 
+                label={field.label} 
+                className={field.required ? 'form-item-required' : ''}
+                style={{visibility: visible}
+            }>{
+                field.children ?
+                <Row>{
+                    field.children.map( (child, idx) =>
+                        <Col key={idx} span={fields[child].span}>
+                            <FormItem>{_this.genDecoratedField(child)}</FormItem>
+                        </Col> )
+                }</Row> : _this.genDecoratedField(col)
+            }</FormItem>
+        )
     }
 
     // Generate antd decorated fiels.
@@ -81,6 +105,14 @@ class Autonomous extends Component{
                     {field.options.map((opt, idx) => <Option value={opt.itemNo} key={idx}>{opt.itemName}</Option>)}
                 </Select>
                 break;
+            case 'radio':
+                el = <RadioGroup {...evts} {...attrs}>
+                    {field.options.map((opt, idx) => <Radio value={opt.itemNo} key={idx}>{opt.itemName}</Radio>)}
+                </RadioGroup>;
+                break;
+            case 'datePicker':
+                el = <DatePicker {...evts} {...attrs}/>
+                break;
             case 'custom':
                 el = field.markup;
                 break;
@@ -90,12 +122,14 @@ class Autonomous extends Component{
 
         // Wrap each field with antd decorator to achive bi-directional data binding.
         return getFieldDecorator(id, field.decorator)(el);
-    } 
+    }
 
     render () {
+        const capStyle = {margin: '20px'};
         return (
             <div>
                 <Form>
+                    <Row><h2 style={capStyle}>{this.props.data.caption}</h2></Row>
                     {this.genRows()}
                 </Form>
             </div>
